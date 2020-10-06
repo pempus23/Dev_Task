2572209b7f4520276a4f304b08d2897deda89f0a
Vitalii <vpempus@gmail.com>
2020-10-07 00:19:36 +0300

    Добавьте файлы проекта.

---
+++ Dev_Task.Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dev_Task.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Type ReaderType { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int NumberOfPages { get; set; }
+        public string Description { get; set; }
+    }
+    public enum Type
+    {
+        Children, Adults
+    }
+}
