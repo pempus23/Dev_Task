12bbaaba7d85ec15678a5d270aa45e06414e8fda
Vitalii <vpempus@gmail.com>
2020-10-09 15:54:09 +0300

    All

--- Dev_Task.Models/Book.cs
+++ Dev_Task.Models/Book.cs
@@ -5,7 +5,6 @@ namespace Dev_Task.Models
 {
     public class Book 
     {   
-        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public Type ReaderType { get; set; }
